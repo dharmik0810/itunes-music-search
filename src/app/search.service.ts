8f9cc34663d1d925a3a01e2c5019f655ae105719
dharmik0810 <dharmik0810@gmail.com>
2018-04-15 19:25:58 -0400

    retrieving data from service and displaying it to UI

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -5,17 +5,25 @@ import 'rxjs/Rx';
 @Injectable()
 export class SearchService {
   apiRoot: string = 'https://itunes.apple.com/search';
-
-  constructor(private http: Http) { }
+  results: Object[];
+  constructor(private http: Http) {
+    this.results = [];
+   }
   
   search(term: String){
     let promise = new Promise((resolve, reject)=>{
       let apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20`;
       this.http.get(apiURL)
         .toPromise()
-        .then(response =>{
-          console.log(response.json());
-        })
+        .then(
+          response =>{
+            this.results = response.json().results;
+            resolve();
+          }, 
+          msg =>{
+            reject(msg);
+          }
+        );
 
     });
     return promise;
